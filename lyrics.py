b0b9a853cbae31a38ad1777871dec06d18126dcd
Lorna Armstrong <43929997+lornaarmstrong@users.noreply.github.com>
2021-05-18 23:30:09 +0100

    Implement Authentication and Current Song

---
+++ lyrics.py
@@ -0,0 +1,24 @@
+# import necessary packages
+import spotipy
+import spotipy.util as util
+from config import USERNAME, SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI
+
+# Set the scope for the script
+scope = 'user-read-currently-playing'
+
+token = util.prompt_for_user_token(USERNAME, scope, client_id=SPOTIPY_CLIENT_ID,
+    client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI)
+
+# check if the token is working
+if token:
+    sp = spotipy.Spotify(auth=token)
+
+    # Get the currently-playing song
+    current_song = sp.currently_playing()
+    artist = current_song['item']['artists'][0]['name']
+    name_song = current_song['item']['name']
+
+    print("Currently Playing:  " + artist, name_song)
+
+else:
+    print("Can't get token for", USERNAME)
